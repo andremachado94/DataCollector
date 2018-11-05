b778efd6f12322d7970b183b602084059ea6568e
Andre Machado <andremachado94@gmail.com>
2018-11-05 11:00:08 +0000

    1st commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=datacollector-9304-Web-Deploy.pubxml -p:Password=ZqWluZXs3xf2Bn86sgTjWEb60wlgaFvlb9S15PMmrBGGvnC3igoExgoKdhHG
+
